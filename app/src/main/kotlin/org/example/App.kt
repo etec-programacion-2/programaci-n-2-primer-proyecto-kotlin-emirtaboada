780baedfc6ecb7e8da2469767f64d5e419ca6401
emirtaboada <e.taboada@alumno.etec.um.edu.ar>
2025-04-22 12:46:51 -0300

    Paso 1: Configuración inicial

---
+++ app/src/main/kotlin/org/example/App.kt
@@ -0,0 +1,8 @@
+package org.example
+
+fun main() {
+    val nombre: String = "emir"
+    var fecha = "22 de abril de 2025"
+    println("Hola mi nombre es $nombre")
+    println("Hoy es  $fecha")
+}
